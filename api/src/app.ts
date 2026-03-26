3b655c9c5f2fa0cc1877d0f168f1a4c51a0ea156
EdwardVNguyen <edwardnguyen568@gmail.com>
2026-03-25 22:39:29 -0500

    created entry point to backend server, app.ts and index.ts, created a modular app file to reuse for testing

---
+++ api/src/app.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config'
+import express from 'express'
+import type { Application, Request, Response } from 'express'
+
+const app: Application = express()
+
+app.use(express.json())
+
+export default app
